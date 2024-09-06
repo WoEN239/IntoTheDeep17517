f398552044397b8f8f8d601c9e21b238716d2739
Egor Khvostikov <egorhvog@gmail.com>
2024-09-06 19:19:24 +0300

    feat(Position): добавлен класс для хранения и работы с позицией

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Position.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Math;
+
+public class Position {
+    public double x;
+    public double y;
+    public double h;
+
+    public Position(double x, double y, double h) {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+    }
+}
