6407d31c55e9387683c02d4da4bbaae838900b8e
Egor Khvostikov <egorhvog@gmail.com>
2024-09-06 19:06:10 +0300

    feat(Task) добавлен класс для хранения действий. Планируется использование для создания последовательности действий и использования ее в автономном режиме.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Events/Task.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Events;
+
+import java.util.function.Supplier;
+
+public class Task {
+    private Runnable run;
+    private Supplier<Boolean> isDone;
+    private final double guardTime;
+    private final double bornTime;
+
+    public Task(Runnable run,Supplier<Boolean> isDone, double guardTime){
+        this.isDone = isDone;
+        this.run = run;
+        this.guardTime = guardTime;
+        this.bornTime = System.currentTimeMillis();
+    }
+    public void run(){
+        run();
+    }
+
+    public boolean isDone(){
+        boolean target =  isDone();
+        return System.currentTimeMillis()-bornTime>guardTime || target;
+    }
+}
