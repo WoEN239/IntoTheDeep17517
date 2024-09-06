4457466703fe3102ff254c8fe3e50b52ebc90630
Egor Khvostikov <egorhvog@gmail.com>
2024-09-06 19:22:54 +0300

    feat(DriveTrain,DriveTrainStatus): добавлены классы для управления скоростью робота и хранения статуса колёсной базы

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Events/DriveTrainStatus.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Events;
+
+public class DriveTrainStatus {
+    public double xPos;
+    public double yPos;
+    public double xVel;
+    public double yVel;
+    public double hPos;
+    public double hVel;
+
+    public double xPosTar;
+    public double yPosTar;
+    public double xVelTar;
+    public double yVelTar;
+    public double hPosTar;
+    public double hVelTar;
+
+    public boolean isAtTarget;
+    public DriveTrainStatus(){}
+}
