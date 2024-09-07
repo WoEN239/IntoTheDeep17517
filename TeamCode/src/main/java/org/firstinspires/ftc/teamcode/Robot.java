2021939694b7e62128f6e774819bbfddbebc6eda
EgorKhvostikov <egorhvog@gmail.com>
2024-09-07 23:08:21 +0300

    feat(Robot, DevicePool) добавлен глобальный таймер

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Devices.DevicePool;
 import org.firstinspires.ftc.teamcode.Events.Task;
 import org.firstinspires.ftc.teamcode.Modules.IModule;
 import org.firstinspires.ftc.teamcode.Modules.ModulesList;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class Robot extends ModulesList {
@@ -15,11 +17,11 @@ public class Robot extends ModulesList {
     private ArrayList<Task> taskQueue = new ArrayList<>();
     public DevicePool devicePool;
     public HardwareMap hardwareMap;
-
+    public ElapsedTime timer = new ElapsedTime();
     public Robot(LinearOpMode opMode){
         this.opMode = opMode;
         this.hardwareMap = opMode.hardwareMap;
-        devicePool = new DevicePool(this.hardwareMap);
+        devicePool = new DevicePool(this);
     }
     public void addToQueue(Task task){
         taskQueue.add(task);
@@ -32,6 +34,7 @@ public class Robot extends ModulesList {
         ) {
             i.init(this);
         }
+        timer.reset();
     }
     public void update(){
         for (IModule i:modules
