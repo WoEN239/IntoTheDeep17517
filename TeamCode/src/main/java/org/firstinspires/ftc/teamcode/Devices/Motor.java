674b7223a6941147d129399690b4d2026ef39c08
EgorKhvostikov <egorhvog@gmail.com>
2024-09-07 23:09:24 +0300

    feat(Motor) попытка написать фильтр скорости. TODO: протестить и подобрать K

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/Motor.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.Devices;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Math.Pid;
 import org.firstinspires.ftc.teamcode.Math.PidStatus;
@@ -13,10 +14,12 @@ public class Motor{
     private String name;
     private DcMotorEx dev;
     private int dir = 1;
-    public Motor(String name, HardwareMap map) {
+    private ElapsedTime timer;
+    public Motor(String name, HardwareMap map, ElapsedTime timer) {
         this.dev = map.get(DcMotorEx.class,name);
         this.id = dev.getPortNumber();
         this.name = dev.getDeviceName();
+        this.timer = timer;
     }
 
     public void setDir(int i){
@@ -42,8 +45,24 @@ public class Motor{
     double getPos(){
         return dev.getCurrentPosition();
     }
+    double timeOld = timer.seconds();
+
+    double velTrueK0 = 0;
+    double velTrueK = 0;
+    public static double K = 1;
     double getVel(){
-        return dev.getVelocity();//TODO filter
+        double tNow = timer.seconds();
+        double dt = tNow- timeOld;
+        double aK = (velTrueK-velTrueK0)/dt;
+
+        double velSensorK1 = dev.getVelocity();
+
+        double velTrueK1 = K*velSensorK1 + (1-K)*(velTrueK + aK*dt);
+        timeOld = tNow;
+        velTrueK0 = velTrueK;
+        velTrueK = velTrueK1;
+
+        return velTrueK1;
     }
     public void updatePid(PidStatus status){
         pidStatus.copyFrom(status);
