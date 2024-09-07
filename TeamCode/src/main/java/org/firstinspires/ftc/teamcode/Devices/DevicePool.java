73b29e808a4f25846bfad8d474e69cc75c21e752
EgorKhvostikov <egorhvog@gmail.com>
2024-09-07 23:08:21 +0300

    feat(Robot, DevicePool) добавлен глобальный таймер

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/DevicePool.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/DevicePool.java
@@ -1,17 +1,17 @@
 package org.firstinspires.ftc.teamcode.Devices;
 
-import com.qualcomm.robotcore.hardware.HardwareDevice;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Robot;
 
 public class DevicePool {
     public Motor rightForwardDrive;
     public Motor rightBackDrive;
     public Motor leftForwardDrive;
     public Motor leftBackDrive;
-    public  DevicePool(HardwareMap map){
-        rightBackDrive = new Motor("",map);
-        rightForwardDrive = new Motor("",map);
-        leftBackDrive = new Motor("",map);
-        leftForwardDrive = new Motor("",map);
+    public  DevicePool(Robot robot){
+        rightBackDrive = new Motor("",robot.hardwareMap,robot.timer);
+        rightForwardDrive = new Motor("",robot.hardwareMap,robot.timer);
+        leftBackDrive = new Motor("",robot.hardwareMap,robot.timer);
+        leftForwardDrive = new Motor("",robot.hardwareMap,robot.timer);
     }
 }
