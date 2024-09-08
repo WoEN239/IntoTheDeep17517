571207e2c2e5572e72321575570cc57f83f29e56
EgorKhvostikov <egorhvog@gmail.com>
2024-09-08 10:30:19 +0300

    fix(Task) исправлена ошибка в вызове функции

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Events/Task.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Events/Task.java
@@ -3,23 +3,24 @@ package org.firstinspires.ftc.teamcode.Events;
 import java.util.function.Supplier;
 
 public class Task {
-    private Runnable run;
-    private Supplier<Boolean> isDone;
+    private final Runnable run;
+    private final Supplier<Boolean> isDone;
     private final double guardTime;
     private final double bornTime;
 
-    public Task(Runnable run,Supplier<Boolean> isDone, double guardTime){
+    public Task(Runnable run,Supplier<Boolean> isDone, double guardTime,double bornTime){
         this.isDone = isDone;
         this.run = run;
         this.guardTime = guardTime;
-        this.bornTime = System.currentTimeMillis();
+        this.bornTime = bornTime;
     }
-    public void run(){
-        run.run();
+    public void run(double timeNowSeconds){
+        if(timeNowSeconds>=bornTime){
+            if(!isDone.get()) {
+                run.run();
+            }
+        }
+
     }
 
-    public boolean isDone(){
-        boolean target =  isDone();
-        return System.currentTimeMillis()-bornTime>guardTime || target;
-    }
 }
