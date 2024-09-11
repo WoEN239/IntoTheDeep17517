c0a7a4c2ce302aacb492348d6939fa782a521ac6
EgorKhvostikov <egorhvog@gmail.com>
2024-09-11 22:37:46 +0300

    feat(Gyro.java, Odometry.java) добавлена одометрия и гироскоп

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/DriveTrain/Gyro.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Modules.DriveTrain;
+
+import static com.qualcomm.hardware.rev.RevHubOrientationOnRobot.xyzOrientation;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Modules.IModule;
+import org.firstinspires.ftc.teamcode.Robot;
+
+public class Gyro implements IModule {
+    private IMU imu;
+    private double angle;
+    private double speed;
+    @Override
+    public void init(Robot robot) {
+        imu = robot.hardwareMap.get(IMU.class,"imu");
+        Orientation hubRotation = xyzOrientation(0,0,0);
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot
+                (RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
+        imu.initialize(new IMU.    Parameters(orientationOnRobot));
+        reset();
+    }
+    public void reset(){
+        imu.resetYaw();
+    }
+
+    @Override
+    public void update() {
+        angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        speed = imu.getRobotAngularVelocity(AngleUnit.DEGREES).xRotationRate;
+    }
+    public double getAngle(){
+        return angle;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/DriveTrain/Odometry.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.Modules.DriveTrain;
+
+import org.firstinspires.ftc.teamcode.Devices.Motor;
+import org.firstinspires.ftc.teamcode.Math.Position;
+import org.firstinspires.ftc.teamcode.Modules.IModule;
+import org.firstinspires.ftc.teamcode.Robot;
+
+public class Odometry implements IModule {
+    public void init(Robot robot){
+        rightm = robot.devicePool.rightOdometer;
+        leftm = robot.devicePool.leftOdometer;
+        ym = robot.devicePool.yOdometer;
+        velRightBackM = robot.devicePool.rightBackDrive;
+        velLeftBackM  = robot.devicePool.leftBackDrive;
+        velRightForM  = robot.devicePool.rightForwardDrive;
+        velLeftForM   = robot.devicePool.leftForwardDrive;
+        this.robot = robot;
+    }
+    Robot robot;
+    private final Position velocityLocal = new Position(0,0,0);
+    private final Position positionLocal = new Position(0,0,0);
+    private final Position velocityGlobal = new Position(0,0,0);
+    private final Position positionGlobal = new Position(0,0,0);
+    Motor rightm;
+    Motor leftm;
+    Motor ym;
+
+    public Position getVelocityLocal() {
+        return velocityLocal;
+    }
+
+    public Position getPositionLocal() {
+        return positionLocal;
+    }
+
+    public Position getVelocityGlobal() {
+        return velocityGlobal;
+    }
+
+    public Position getPositionGlobal() {
+        return positionGlobal;
+    }
+
+    Position deltaPosition;
+    private void calcLocalPosition(){
+        double x = ym.dev.getCurrentPosition()                                            ;
+        double y = (rightm.dev.getCurrentPosition() + leftm.dev.getCurrentPosition()) /2.0;
+        double h = (rightm.dev.getCurrentPosition() - leftm.dev.getCurrentPosition()) /2.0;
+        deltaPosition = new Position(x,y,h);
+        deltaPosition.minus(positionLocal);
+        positionLocal.y = y;
+        positionLocal.x = x;
+        positionLocal.h = h;
+    }
+    private void calcGlobalPosition(){
+        positionGlobal.h = robot.imu.getAngle();
+        deltaPosition.rotateIt(positionGlobal.h);
+        positionGlobal.plus(deltaPosition);
+    }
+    Motor velRightBackM;
+    Motor velLeftForM;
+    Motor velRightForM;
+    Motor velLeftBackM;
+    Position deltaVel;
+    public void calcLocalVelocity(){
+        double x = (velRightBackM.getVel()+velRightForM.getVel()+velLeftBackM.getVel()+velLeftForM.getVel())/4.0;
+        double y = (velRightBackM.getVel()-velRightForM.getVel()-velLeftBackM.getVel()+velLeftForM.getVel())/4.0;
+        double h = (-velRightBackM.getVel()-velRightForM.getVel()+velLeftBackM.getVel()+velLeftForM.getVel())/4.0;
+        deltaVel = new Position(x,y,h);
+        deltaVel.minus(velocityLocal);
+        velocityLocal.x = x;
+        velocityLocal.y = y;
+        velocityLocal.h = h;
+    }
+    public void calcGlobalVelocity(){
+        velocityGlobal.h = robot.imu.getSpeed();
+        deltaVel.rotateIt(positionGlobal.h);
+        positionGlobal.plus(deltaVel);
+    }
+    @Override
+    public void update(){
+        calcLocalPosition();
+        calcLocalVelocity();
+        calcGlobalPosition();
+        calcGlobalVelocity();
+    }
+}
