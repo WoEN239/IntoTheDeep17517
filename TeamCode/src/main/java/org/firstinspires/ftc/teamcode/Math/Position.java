41b3b81a683c8994d4883a19a5602fa3ab138101
EgorKhvostikov <egorhvog@gmail.com>
2024-09-11 22:35:37 +0300

    feat(Task) улучшено хранение задач и состояний

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Position.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Math;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class Position {
     public double x;
     public double y;
@@ -10,4 +13,20 @@ public class Position {
         this.y = y;
         this.h = h;
     }
+    public void rotateIt(double degreesAngle){
+        double angle1 = Math.toRadians(degreesAngle);
+        double x1 = x * cos(angle1) - y * sin(angle1);
+        double y1 = x * sin(angle1) + y * cos(angle1);
+        x = x1;
+        y = y1;
+    }
+    public void  minus(Position pos){
+        x -= pos.x;
+        y -= pos.y;
+    }
+    public void plus(Position pos){
+        x += pos.x;
+        y += pos.y;
+    }
+
 }
