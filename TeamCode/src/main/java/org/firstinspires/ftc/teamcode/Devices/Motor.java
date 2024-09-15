b0cd1ecae27867afc6feaf4e2d4a94ea96601df1
EgorKhvostikov <egorhvog@gmail.com>
2024-09-15 13:09:55 +0300

    feat(Motor) изменён фильтр

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/Motor.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode.Devices;
 
+import static java.lang.Math.abs;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+
 import org.firstinspires.ftc.teamcode.Math.Pid;
 import org.firstinspires.ftc.teamcode.Math.PidStatus;
+
+import java.util.Arrays;
+
+
 @Config
 public class Motor{
     public final DcMotorEx dev;
@@ -15,7 +22,7 @@ public class Motor{
         this.dev = map.get(DcMotorEx.class,name);
 
     }
-    ElapsedTime timer = new ElapsedTime();
+
     public void setDir(int i){
         if (i == 1 || i ==-1) {
             dir = i;
@@ -23,48 +30,67 @@ public class Motor{
     }
     PidStatus pidStatus = new PidStatus(0,0,0,0,0,0);
     Pid pid = new Pid(pidStatus);
-    double posOld = 0;
     public void setVel(double vel){
-
-        double u = pid.calc(vel,this.getVel());
-
-
+        double u = pid.calc(vel,getPos());
         setPower(u);
     }
     public void setPower(double power){
         dev.setPower(power*dir);
     }
     double getPos(){
-        return dev.getCurrentPosition();
+        return dir*dev.getCurrentPosition();
     }
-    double velL = 0;
-    double tOld = 0;
-    public double getVel(){
-        double tNow = timer.seconds();
-        if(tNow - tOld > 1/1000.0) {
-            double posNow = dev.getCurrentPosition();
-            double vel = filter((posNow - posOld) / (tNow - tOld));
-            tOld = tNow;
-            posOld = posNow;
-            velL = vel;
-        }
-        return dir * velL;
-    }
-
     public void updatePid(PidStatus status){
         pidStatus.copyFrom(status);
     }
-    static public int K = 100;
-    double [] lastVel = new double[K];
-    int n = 0;
+    /////////////////////////////////
 
-    private double filter(double i){
-        lastVel[n] = i;
-        n = (n+1)%K;
-        i = 0;
-        for(int j = 0; j<K;j++){
-            i = i +lastVel[j];
-        }
-        return i/K;
+    public static int K  = 5;
+    public static double senseUp = 50;
+    public static double bigK    = 0.3;
+    public static double smallK  = 0.9;
+    private double posOld = 0;
+    private double tOld   = 0;
+    private final double [] lastReads = new double[2*K+1];
+    ElapsedTime timer = new ElapsedTime();
+
+    public double getVel(){
+        updateVel();
+        filter();
+        return vel*dir;
     }
+
+    public void updateVel(){
+        double tNow = timer.seconds();
+        double dt = tNow-tOld;
+        tOld = tNow;
+
+        double posNow = getPos();
+        double dp = posNow - posOld;
+        posOld = posNow;
+
+        for(int i =0; i<(2*K); i++){
+            lastReads[i] = lastReads[i++];
+        }
+
+        lastReads[2*K] = dp/dt;
+    }
+    private double median(){
+        double [] sortReads = Arrays.stream(lastReads).sorted().toArray();
+        return sortReads[K+1];
+    }
+    double vel = 0;
+    private void filter(){
+        double k = 0;
+        double vNow = median();
+        if(abs(vNow - vel)>senseUp){
+            k = bigK;
+        }else{
+            k = smallK;
+        }
+        vel = vel + (vNow - vel)*k;
+    }
+
+
+
 }
