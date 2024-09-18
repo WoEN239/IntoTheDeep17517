93b0be277d2a28b6ed1b92ec56b3ec97fa4824c7
EgorKhvostikov <egorhvog@gmail.com>
2024-09-18 18:02:44 +0300

    feat(Motor) доработан адаптивный коефицент фильтра, сделаны графики

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -7,14 +7,19 @@ import org.firstinspires.ftc.teamcode.Math.Position;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 @Config
 public class TeleOp extends BaseOpMode{
-
-    public static double u;
+    public static boolean isSin = false ;
+    public static double u = 0;
     public void doing(){
         Position target = new Position(rightStickY,rightStickX,leftStickX);
-        robot.devicePool.leftBackDrive.setPower(u);
+        if(isSin){
+            robot.devicePool.leftBackDrive.setPower(Math.sin(robot.timer.seconds()*u));
+        }else {
+            robot.devicePool.leftBackDrive.setPower(u);
+        }
 
         this.telemetry.addData("vel",robot.devicePool.leftBackDrive.getVel());
         FtcDashboard.getInstance().getTelemetry().addData("vel",robot.devicePool.leftBackDrive.getVel());
+        FtcDashboard.getInstance().getTelemetry().addData("u",Math.sin(robot.timer.seconds()*u));
         FtcDashboard.getInstance().getTelemetry().addData("velFrom getVelocity()",robot.devicePool.leftBackDrive.dev.getVelocity());
         FtcDashboard.getInstance().getTelemetry().update();
 
