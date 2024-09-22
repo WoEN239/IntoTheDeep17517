1b777bade696cac5e434bc3502f89caad2280f2b
EgorKhvostikov <egorhvog@gmail.com>
2024-09-22 19:28:40 +0300

    feat(Viewers) одометрия разбита на классы

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/DriveTrain/PositionViewer/PositionLocalViewer.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Modules.DriveTrain.PositionViewer;
+
+import org.firstinspires.ftc.teamcode.Devices.Motor;
+import org.firstinspires.ftc.teamcode.Math.Position;
+import org.firstinspires.ftc.teamcode.Robot;
+
+public class PositionLocalViewer {
+    public void init(Robot robot){
+        rightOdometer = robot.devicePool.rightOdometer;
+        leftOdometer = robot.devicePool.leftOdometer;
+        yOdometer = robot.devicePool.yOdometer;
+    }
+    private final Position positionLocal = new Position(0,0,0);
+    private Motor rightOdometer;
+    private Motor leftOdometer;
+    private Motor yOdometer;
+    public Position getPositionLocal() {
+        return positionLocal;
+    }
+    public Position deltaPosition;
+    private void calcLocalPosition(){
+        double x = yOdometer.dev.getCurrentPosition()                                            ;
+        double y = (rightOdometer.dev.getCurrentPosition() + leftOdometer.dev.getCurrentPosition()) /2.0;
+        double h = (rightOdometer.dev.getCurrentPosition() - leftOdometer.dev.getCurrentPosition()) /2.0;
+        deltaPosition = new Position(x,y,h);
+        deltaPosition.minus(positionLocal);
+        positionLocal.y = y;
+        positionLocal.x = x;
+        positionLocal.h = h;
+    }
+
+
+    public void update(){
+        calcLocalPosition();
+    }
+}
