f78b612ec475d1fff85f7caca1f1939cd1e35b39
EgorKhvostikov <egorhvog@gmail.com>
2024-09-23 21:21:19 +0300

    feat(RoadRunner) он явился! (добавлен рр)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/RoadRunnerTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.OpModes.TestOpmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Events.Task;
+import org.firstinspires.ftc.teamcode.Math.Position;
+import org.firstinspires.ftc.teamcode.OpModes.BaseOpMode;
+
+@Autonomous
+public class RoadRunnerTest extends BaseOpMode {
+    @Override
+    public void doing(){
+        robot.addToQueue(new Task(
+                ()->true,
+                ()->false,
+                new Runnable[]{
+                ()-> robot.roadRunner.moveToTrajectory(
+                     robot.roadRunner.builder()
+                             .lineToX(10)
+                             .build()
+                )
+                },
+                new Runnable[]{
+                ()-> robot.velocityController.move(new Position())
+                }
+        ));
+    }
+}
