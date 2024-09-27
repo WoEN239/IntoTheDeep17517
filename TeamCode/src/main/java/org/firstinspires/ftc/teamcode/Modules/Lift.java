abb2f3814446095c90dd32b3999ead048850bcb2
EgorKhvostikov <egorhvog@gmail.com>
2024-09-27 14:09:03 +0300

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Lift.java
@@ -1,7 +1,13 @@
 package org.firstinspires.ftc.teamcode.Modules;
 
+import org.firstinspires.ftc.teamcode.Robot;
+
 public class Lift implements IModule {
     public Lift(){}
 
 
+    @Override
+    public void init(Robot robot) {
+
+    }
 }
