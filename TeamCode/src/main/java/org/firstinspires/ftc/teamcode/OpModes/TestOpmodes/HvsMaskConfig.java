497e2210064fdd4709a78ade7106ff84e577a12e
MrFrosty1234 <mitrusinmaksim@gmail.com>
2024-09-28 22:07:50 +0300

    feat(all null objects) )))

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/HvsMaskConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/HvsMaskConfig.java
@@ -3,13 +3,18 @@ package org.firstinspires.ftc.teamcode.OpModes.TestOpmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.OpModes.BaseOpMode;
+import org.firstinspires.ftc.teamcode.R;
+import org.firstinspires.ftc.teamcode.Robot;
 
 @Autonomous(group = "Test")
 public class HvsMaskConfig  extends BaseOpMode {
     {
         isCamera = true;
     }
+
     @Override
+
+
     public void doing() {
 
     }
