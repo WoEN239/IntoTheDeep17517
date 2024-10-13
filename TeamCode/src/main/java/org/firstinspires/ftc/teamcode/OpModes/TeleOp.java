998ac4271f98b55c699f36ac5e8401a04731fb45
EgorKhvostikov <egorhvog@gmail.com>
2024-10-13 15:49:02 +0300

    fix исправлены некоторые конфликты между версиями Egor Khvostikov и MrFrosty

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -1,23 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModes;
-import com.acmerobotics.dashboard.FtcDashboard;
-
-import org.firstinspires.ftc.teamcode.Math.Position;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends BaseOpMode{
-    public static boolean isSin = false ;
-    public static double u = 0;
     public void doing(){
-        Position target = new Position(rightStickY,rightStickX,leftStickX);
-        if(isSin){
-            robot.devicePool.leftBackDrive.dev.setPower(Math.sin(robot.timer.seconds())*u);
-        }else {
-            robot.devicePool.leftBackDrive.setPower(u);
-        }
-
-
-        FtcDashboard.getInstance().getTelemetry().addData("vel",robot.devicePool.leftBackDrive.getVelocity());
-        FtcDashboard.getInstance().getTelemetry().addData("u",u);
-        FtcDashboard.getInstance().getTelemetry().update();
-
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/TestFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/TestFilter.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.OpModes.TestOpmodes;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.Devices.DevicePool;
 import org.firstinspires.ftc.teamcode.OpModes.BaseOpMode;
 
 @Config
@@ -15,11 +13,11 @@ public class TestFilter extends BaseOpMode {
     public static double a = 0;
     public void doing(){
         if(isSin){
-            DevicePool.leftBackDrive.setPower(Math.sin(robot.timer.seconds()/Math.PI*u)*a);
+            robot.devicePool.driveTrainMotors.leftBackDrive.setPower(Math.sin(robot.timer.seconds()/Math.PI*u)*a);
         }else {
-            DevicePool.leftBackDrive.setPower(u);
+            robot.devicePool.driveTrainMotors.leftBackDrive.setPower(u);
         }
-        FtcDashboard.getInstance().getTelemetry().addData("Rev Vel", DevicePool.leftBackDrive.dev.getVelocity());
+        FtcDashboard.getInstance().getTelemetry().addData("Rev Vel", robot.devicePool.driveTrainMotors.leftBackDrive.dev.getVelocity());
         FtcDashboard.getInstance().getTelemetry().addData("u",u);
         FtcDashboard.getInstance().getTelemetry().update();
 
