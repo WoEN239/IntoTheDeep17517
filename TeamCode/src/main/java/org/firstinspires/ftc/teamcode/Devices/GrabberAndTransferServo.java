d8bbd0cb68deb3b74b44e7df0e9c9ec23491dac6
EgorKhvostikov <egorhvog@gmail.com>
2024-10-22 18:22:54 +0300

    fix исправлен ряд NPE в объявлении девайсов

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/GrabberAndTransferServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devices/GrabberAndTransferServo.java
@@ -3,22 +3,22 @@ package org.firstinspires.ftc.teamcode.Devices;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/**
- * Writing by @MrFrosty1234
- */
+/*
+ Writing by @MrFrosty1234
+*/
 
 public class GrabberAndTransferServo {
 
     public static Servo grabberServo       ;
-    public static Servo grabberServoUp     ;
-    public static Servo transferServoFirst ;
+    public static Servo flipServo          ;
+    public static Servo transferServo      ;
     public static Servo transferServoSecond;
 
 
     public GrabberAndTransferServo(HardwareMap hardwareMap) {
         grabberServo = hardwareMap.get(Servo.class, "fakeServo");
-        transferServoFirst = hardwareMap.get(Servo.class, "fakeServo");
+        transferServo = hardwareMap.get(Servo.class, "fakeServo");
         transferServoSecond = hardwareMap.get(Servo.class, "fakeServo");
-        grabberServoUp = hardwareMap.get(Servo.class, "fakeServo");
+        flipServo = hardwareMap.get(Servo.class, "fakeServo");
     }
 }
