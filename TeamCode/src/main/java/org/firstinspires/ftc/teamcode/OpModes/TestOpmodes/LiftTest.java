46023d959736543e302ace25b8a15b34ec30df2f
MrFrosty1234 <mitrusinmaksim@gmail.com>
2024-11-01 16:59:15 +0300

    fix() не увидел скрбку

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpmodes/LiftTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.OpModes.TestOpmodes;
+
+import org.firstinspires.ftc.teamcode.Devices.LiftHangingMotors;
+import org.firstinspires.ftc.teamcode.Modules.Intake.Lift.LiftController;
+import org.firstinspires.ftc.teamcode.Modules.Intake.Lift.LiftListener;
+import org.firstinspires.ftc.teamcode.OpModes.BaseOpMode;
+
+public class LiftTest extends BaseOpMode {
+    @Override
+    public void doing() {
+
+    }
+}
