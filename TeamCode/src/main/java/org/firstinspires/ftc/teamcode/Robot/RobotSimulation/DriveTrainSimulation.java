c5936fce78cb299391056841386e1c86bc9d9f51
EgorKhvostikov <egorhvog@gmail.com>
2024-12-25 20:33:23 +0300

    temporary commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotSimulation/DriveTrainSimulation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotSimulation/DriveTrainSimulation.java
@@ -5,16 +5,29 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Math.Position;
 
 public class DriveTrainSimulation {
-    public static Position position = new Position();
-    public static Position velocity = new Position();
-
+    public static  Position position      = new Position();
+    public static  Position localPosition = new Position();
+    public static  Position velocity      = new Position();
+    private static Position oldPosition   = new Position();
     static ElapsedTime time = new ElapsedTime();
     public static void updatePosition(){
         if(time.seconds()>0.02) {
-            position.x += velocity.x * time.seconds();
-            position.y += velocity.y * time.seconds();
-            position.h += velocity.h * time.seconds();
+            localPosition.x += velocity.x * time.seconds();
+            localPosition.y += velocity.y * time.seconds();
+            localPosition.h += velocity.h * time.seconds();
+
+            Position dp = new Position().copyFrom(localPosition).vectorMinus(oldPosition);
+            oldPosition.copyFrom(localPosition);
+            dp.rotateVector(position.h);
+            position.vectorPlus(dp);
+            position.h = localPosition.h;
             time.reset();
         }
+    }
+    public static void reset(){
+        position      = new Position();
+        localPosition = new Position();
+        velocity      = new Position();
+        oldPosition   = new Position();
     }
 }
